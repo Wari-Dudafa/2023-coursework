8ceef67a04385c7f9015731d51535a009a9c3bb9
Wari-Dudafa <60980271+Wari-Dudafa@users.noreply.github.com>
2022-04-26 12:41:10 +0100

    Update install.php

--- install.php
+++ install.php
@@ -8,7 +8,7 @@ CREATE TABLE TblVideos
 (VideoID INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 VideoTitle VARCHAR(50) NOT NULL,
 Likes INT(10) NOT NULL,
-Dislikes INT(10) NOT NULL,)");
+Dislikes INT(10) NOT NULL)");
 $stmt->execute();
 $stmt->closeCursor();
 
@@ -16,21 +16,21 @@ $stmt = $conn->prepare("DROP TABLE IF EXISTS TblUsers;
 CREATE TABLE TblUsers 
 (UserID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 Username VARCHAR(20) NOT NULL,
-Password VARCHAR(20) NOT NULL,)");
+Password VARCHAR(20) NOT NULL)");
 $stmt->execute();
 $stmt->closeCursor();
 
 $stmt = $conn->prepare("DROP TABLE IF EXISTS TblUsersVideos;
 CREATE TABLE TblUsersVideos 
-(UserID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-VideoID INT(9) UNSIGNED AUTO_INCREMENT NOT NULL,)");
+(UserID INT(6) NOT NULL,
+VideoID INT(9) NOT NULL)");
 $stmt->execute();
 $stmt->closeCursor();
 
 $stmt = $conn->prepare("DROP TABLE IF EXISTS TblTags;
 CREATE TABLE TblTags 
 (VideoID INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-Tag INT(10) NOT NULL,)");
+Tag INT(10) NOT NULL)");
 $stmt->execute();
 $stmt->closeCursor();
 ?>
