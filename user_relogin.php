feb7a5629f4925dc0ef893c4cc190ea16695c95c
Wari-Dudafa <60980271+Wari-Dudafa@users.noreply.github.com>
2022-04-29 12:06:11 +0100

    Update user_relogin.php

--- user_relogin.php
+++ user_relogin.php
@@ -1,1 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>Login/Sign up</title>
+    
+</head>
+<body>
+    <p1> Login falied, please sign up or try again </p><br>
+      
+<form action="adduser.php" method="post">
+  Username:<input type="text" name="Username"><br>
+  Password:<input type="password" name="Password"><br>
+  <input type="submit" value="Sign up">
+</form>
 
+<form action="loginuser.php" method="post">
+  Username:<input type="text" name="Username"><br>
+  Password:<input type="password" name="Password"><br>
+  <input type="submit" value="Login">
+</form>
+ 
+</body>
+</html>
