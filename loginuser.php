2b280be2395b4efc06bba761c972c9f70a76fa26
Wari-Dudafa <60980271+Wari-Dudafa@users.noreply.github.com>
2022-05-02 08:58:14 +0100

    Update loginuser.php

--- loginuser.php
+++ loginuser.php
@@ -1,18 +1,23 @@
 <?php
 include_once ("connection.php");
-//header('Location: login.php');
 print_r($_POST);
 array_map("htmlspecialchars", $_POST);
+$hashed_password = password_hash($_POST["Password"], PASSWORD_DEFAULT);
+
 $stmt = $conn->prepare("SELECT * FROM tblusers WHERE Username =:Username ;" );
 $stmt->bindParam(':Username', $_POST['Username']);
 $stmt->execute();
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
 { 
-    if($row['Password']== $_POST['Password']){
-        header('Location: placeholder.php');
+    if($row['Password']== $hashed_password){
+        //header('Location: placeholder.php');
+        echo $_POST["Username"]."<br>";
+        echo $hashed_password."<br>";
         
     }else{
-        header('Location: user_relogin.php');
+        //header('Location: user_relogin.php');
+        echo $_POST["Username"]."<br>";
+        echo $hashed_password."<br>";
     }
 }
 $conn=null
