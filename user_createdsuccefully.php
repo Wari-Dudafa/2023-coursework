d336f431d3ed3ac251d167ee449082016b83c5dc
Wari-Dudafa <60980271+Wari-Dudafa@users.noreply.github.com>
2022-05-04 20:13:59 +0100

    Update user_createdsuccefully.php

--- user_createdsuccefully.php
+++ user_createdsuccefully.php
@@ -4,22 +4,45 @@
     
     <title>Login/Sign up</title>
     <link rel="icon" type="image/x-icon" href="BranchLogo.png">
+    <link rel="stylesheet" href="style.css">
     
 </head>
 <body>
-  <p1> Account created succesfully, please login </p><br>
-      
-  <form action="adduser.php" method="post">
-    Username:<input type="text" name="Username"><br>
-    Password:<input type="password" name="Password"><br>
-    <input type="submit" value="Sign up">
-  </form>
+<nav>
 
-  <form action="loginuser.php" method="post">
-    Username:<input type="text" name="Username"><br>
-    Password:<input type="password" name="Password"><br>
-    <input type="submit" value="Login">
-  </form>
+  <div class="topnav">
+      <div class="icon">   
+          <a href="placeholder.php"> <!--Change me to a homepage-->
+                  <img src="BranchLogo.png" alt="my picture" height="45" width="45" />
+              </a>
+      </div>
+
+  </div>
+</nav>
+  <h1><center>Account created succesfully! Please login</center></h1>
+  <div class="loginsignup">
+    <div class="loginsignupchild">
+      <h1>Sign up</h1>
+      <form action="adduser.php" method="post">
+        Username:<input type="text" name="Username" style="margin-bottom: 10px;"><br>
+        Password:<input type="password" name="Password" style= "margin-bottom: 10px;"><br>
+        <input type="submit" value="Sign up">
+      </form>
+    </div>
+
+    <div class="loginsignupchild">
+    <h1>Log in</h1>
+      <form action="loginuser.php" method="post">
+        Username:<input type="text" name="Username" style="margin-bottom: 10px;"><br>
+        Password:<input type="password" name="Password" style="margin-bottom: 10px;"><br>
+        <input type="submit" value="Login">
+      </form>
+    </div>
+  </div>
+
+  <footer>
+        <!---->
+    </footer>
  
 </body>
 </html>
