90c5ba63c8d51ac4e5219dcc1037a63e9eafc456
Wari-Dudafa <60980271+Wari-Dudafa@users.noreply.github.com>
2022-05-10 09:58:41 +0100

    Update placeholder.php

--- placeholder.php
+++ placeholder.php
@@ -1,47 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>placeholder</title>
+    <title>placeholder_bootstrap</title>
     <link rel="icon" type="image/x-icon" href="BranchLogo.png">
-    <link rel="stylesheet" href="style.css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
-<body>
-    <nav>
+<body> 
 
-        <div class="topnav">
-            <div class="icon">   
-                <a href="placeholder.php"> <!--Change me to a homepage-->
-                    <img src="BranchLogo.png" alt="my picture" height="45" width="45" />
-                </a>
+    <nav class="navbar navbar-inverse" style="background-color: #002f63;">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a href="#"><img src="BranchLogo.png" alt="icon" width="45" height="45"></a> 
             </div>
-            <div class="userinfo">
-                <a href="upload.php"><?php
-                    session_start();
-                    if (!isset($_SESSION['CurrentUser']))
-                    {   
-                        header("Location:user.php");
-                        echo "Please login to continue<br>";
-                    }else{
-                        //echo "Access granted<br>";
-                        echo "Current user: " . $_SESSION["CurrentUser"]."<br>";
-                    }
-                ?></a>
-                <a href="logout.php">Logout</a>
-            </div>
-            <div class="search-container">
-                <form action="search_results.php" method="post">
-                    <input type="text" placeholder="Search Branch..." name="search">
-                    <button type="submit"><i class="fa">&#x1F50E;&#xFE0E;</i></button>
-                </form>
-            </div>
-
+            <form class="navbar-form navbar-left" action="#">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search branch..." name="search">
+                    <div class="input-group-btn">
+                    <button class="btn btn-default" type="submit">
+                        <i class="glyphicon glyphicon-search"></i>
+                    </button>
+                    </div>
+                </div>
+            </form>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="upload.php"> <span class="glyphicon glyphicon-upload"></span> Upload</a></li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-user"></span>
+                        <?php
+                            session_start();
+                            if (!isset($_SESSION['CurrentUser']))
+                            {   
+                                header("Location:user.php");
+                                echo "Please login to continue<br>";
+                            }else{
+                                //echo "Access granted<br>";
+                                echo "" . $_SESSION["CurrentUser"];
+                            }
+                        ?>
+                    <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="logout.php">Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
         </div>
-    </nav>  
+    </nav>
 
     <h1>Homepage placeholder</h1>
 
-    <footer>
-        <!--<p></p>-->
-    </footer>
+    <nav class="navbar navbar-inverse navbar-fixed-bottom" style="background-color: #970830;">
+
 </body>
 </html>
