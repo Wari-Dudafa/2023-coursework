67198db6ce16aa228f4409293b01a756bc4bac7a
Wari <waripamooweidudafa@gmail.com>
2022-08-12 14:17:28 +0100

    video upload mark 4
    SUCCESS!!!!

--- upload.php
+++ upload.php
@@ -4,6 +4,7 @@
     <title>Upload</title>
     <link rel="icon" type="image/x-icon" href="BranchLogo.png">
     <?php
+        session_start();
         include_once("connection.php");
 
         if(isset($_POST['but_upload'])){
@@ -29,15 +30,20 @@
 
                     //UPLOADING BIT
                     echo "Still working!";
-                    //***if (move_uploaded_file($_FILES['file']['tmp_name'],$target_file)) {
+                    if (move_uploaded_file($_FILES['file']['tmp_name'],$target_file)) {
                         
                         //insert into database
                         //***$query = "INSERT INTO tblVideos(filename, location) VALUES('".$name."','".$target_file."')";
-
                         //***mysqli_query($con,$query);
 
-                        //***echo "upload done!";
-                    //***}
+                        $stmt = $conn->prepare("INSERT INTO TblVideos (VideoID,VideoTitle,FileName,Location)VALUES (null,:videotitle,'".$name."','".$target_file."')");
+                        $stmt->bindParam(':videotitle', $_POST["Videotitle"]);
+                        $stmt->execute();
+                        $conn=null;
+                        echo $_POST["Videotitle"]."<br>";
+
+                        echo "upload done!";
+                    }
                 }
             }else{
                 echo "file error";
@@ -51,8 +57,9 @@
 <body>
 
     <form method="post" action="" enctype="multipart/form-data">
-        <input type='file' name='file'>
-        <input type="submit" name="but_upload" value="upload">
+        <input type="text" name="Videotitle" value="Video title"><br>
+        <input type='file' name='file'><br>
+        <input type="submit" name="but_upload" value="upload"><br>
 
     </form>
 
--- watchvideo.php
+++ watchvideo.php
@@ -9,17 +9,31 @@
         <?php
 
             include_once("connection.php");
-            $fetchVideos = mysqli_query($con, "SELECT * FROM tblvideos ORDER BY videoid DESC");
-            while ($row = mysqli_fetch_assoc($fetchVideos)) {
-                $location = $row['location'];
-                
+            $stmt = $conn->prepare("SELECT * FROM tblvideos ORDER BY videoid DESC");
+            $stmt->execute();
+            $conn=null;
+            
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+                $location = $row['Location'];
+                $VideoTitle = $row['VideoTitle'];
+                $Likes = $row['Likes'];
+                $Dislikes = $row['Dislikes'];
+
                 echo "<div>";
+                echo "<p>$VideoTitle</p>";
+                echo "<p>likes: $Likes</p>";
+                echo "<p>Dislikes: $Dislikes</p>";
+                echo "<p>Uploaded by: --</p>";
                 echo "<video src='".$location."' controls width='320px' height='200px'>";
                 echo "</div>";
+                break;
+
             }
 
         ?>
+
+        <input type="button" value="Like" onclick="location='test.php'" />
+        <input type="button" value="Dislike" onclick="location='test.php'" />
     </div>
-    
 </body>
 </html>
