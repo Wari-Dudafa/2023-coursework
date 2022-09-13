a00ec14edf2a9dc910c12b926217f718f7c87101
Wari-Dudafa <dudafa.wj@oundleschool.org.uk>
2022-09-13 14:17:19 +0100

    display comments

--- comments.php
+++ comments.php
@@ -5,16 +5,28 @@
     $comment = $_POST["comment"];
     $videoid = $_POST["videoid"];
 
-    echo($userid. "<br>");
-    echo($comment. "<br>");
-    echo($videoid. "<br>");
+    echo("UserID:" .$userid. "<br>");
+    echo("Comment:" .$comment. "<br>");
+    echo("VideoID:" .$videoid. "<br>");
 
     $stmt = $conn->prepare("INSERT INTO TblComments (UserID,VIdeoID,Comment)VALUES (:userid,:videoid,:comment)");
     $stmt->bindParam(':userid', $userid);
     $stmt->bindParam(':comment', $comment);
     $stmt->bindParam(':videoid', $videoid);
     $stmt->execute();
-    $conn=null;
 
-    header("Location: videopage.php");
+    //< Display comments
+        $getcomments = $conn->prepare("SELECT * FROM tblcomments WHERE VideoID = :videoid ;");
+        $getcomments->bindParam(':videoid', $_POST['VideoID']);
+        $getcomments->execute();
+
+        while ($row5 = $stmt->fetch(PDO::FETCH_ASSOC)){
+            $comment = $row5['Comment'];
+            echo "<h3 style='font-size:15px'>'".$comment."'</h3>";
+            echo ($comment);
+        }
+    //>
+    
+    $conn=null;
+    //header("Location: videopage.php");***
 ?>
