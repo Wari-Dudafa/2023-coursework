4f2afe70113f29f12112b1ec7b44893412166e52
Wari-Dudafa <dudafa.wj@oundleschool.org.uk>
2023-02-25 10:51:34 +0000

    DATABASE_CONNECTION

--- connection.php
+++ connection.php
@@ -7,7 +7,7 @@
     $dbname = "dtbBranch";
 
     // Variables for the serverside database connections
-    //$servername = "4023696_dtbbranch";
+    //$servername = "fdb32.awardspace.net";
     //$username = "4023696_dtbbranch";
     //$password = "C]_S8WwO8Tb8p0xT";
     //$dbname = "4023696_dtbbranch";
@@ -17,11 +17,11 @@
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         // Set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
         header("Location:user.php");
     }
     catch(PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
-        header("Location:user.php");
         // Connnection fail
     }
 ?>
