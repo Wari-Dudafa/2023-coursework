5710de8be168b8c3ecf3e0c9b91670127ca1aed4
Wari-Dudafa <dudafa.wj@oundleschool.org.uk>
2023-03-01 17:04:33 +0000

    FIX

--- searchresults.php
+++ searchresults.php
@@ -42,7 +42,7 @@
                 
                 //Partial search to make sure the user can search for things even when spelled differently
                 $partialsearch = "%" . $_GET['search'] . "%";
-                $stmt = $conn->prepare("SELECT * FROM tblvideos WHERE Videotitle LIKE :search;" );
+                $stmt = $conn->prepare("SELECT * FROM TblVideos WHERE Videotitle LIKE :search;" );
                 $stmt->bindParam(':search', $partialsearch);
                 $stmt->execute();
 
@@ -56,7 +56,7 @@
                     $userid = $row['UserID'];
 
                     // Gets the uploader
-                    $stmt2 = $conn->prepare("SELECT * FROM tblusers WHERE UserID =:Userid;");
+                    $stmt2 = $conn->prepare("SELECT * FROM TblUsers WHERE UserID =:Userid;");
                     $stmt2->bindParam(':Userid', $userid);
                     $stmt2->execute();
 
